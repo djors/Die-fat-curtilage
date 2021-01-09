b55d2d92666cb35706321cdc531072b9d6be0c54
asura <843971547@qq.com>
2021-01-09 15:26:56 +0800

    修改项目路径

---
+++ core-server/src/main/java/com/fat/curtilage/CurtilageApplication.java
@@ -0,0 +1,13 @@
+package com.fat.curtilage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurtilageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurtilageApplication.class, args);
+    }
+
+}
---
+++ core-server/src/test/java/com/fat/curtilage/CurtilageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fat.curtilage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurtilageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
