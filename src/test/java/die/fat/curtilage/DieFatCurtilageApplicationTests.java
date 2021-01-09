32eb5b0c23859acf55780036be677b69c5f7cf80
asura <843971547@qq.com>
2021-01-09 15:02:05 +0800

    初始化

---
+++ src/test/java/die/fat/curtilage/DieFatCurtilageApplicationTests.java
@@ -0,0 +1,13 @@
+package die.fat.curtilage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DieFatCurtilageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
